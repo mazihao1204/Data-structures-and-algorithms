76b9727c6894a2bf39e6155d445c4869032a10fb
mazihao1204 <791684511@qq.com>
2020-07-09 13:49:57 +0800

    'bloom'

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
+#### 数据结构与算法
+
+++++
+
+[布隆过滤器](/src/algorithms/布隆过滤器.md)
+
+
+
 #### leetcode 刷题记录
+
+++++
 
 [leecode011 Container With Most Water](/src/array/盛水最多的容器.md)
 
--- src/test.js
+++
@@ -1,9 +0,0 @@
-const maxArea = function(nums){
-  let i = 0
-  let j = nums.length
-  let res = 0
-  while(i<j){
-    res = nums[i] < nums[j] ? Math.max(res,(j - i) * nums[i++]):Math.max(res,(j-i)*nums[j--])
-  }
-  return res
-}
